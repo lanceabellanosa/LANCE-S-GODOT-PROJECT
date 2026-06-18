d85e13f8d236750714e0c80d16d44ea5822a04e5
lanceabellanosa <lance.abellanosa@cathcollege.school.nz>
2026-06-18 13:22:43 +1200

    godot files

--- scripts/question_1.gd
+++ scripts/question_1.gd
@@ -2,4 +2,8 @@ extends Node2D
 
 
 func _on_button_pressed() -> void:
-	
+	get_tree().change_scene_to_file("res://scenes/floor_4.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
