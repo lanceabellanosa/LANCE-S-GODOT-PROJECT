cd50353892ac5da581a01d30c11c8afda0ea2984
lanceabellanosa <lance.abellanosa@cathcollege.school.nz>
2026-06-22 09:31:31 +1200

    godot files

--- scenes/player2.gd
+++ scenes/player2.gd
@@ -77,4 +77,4 @@ func _headbob(time) -> Vector3:
 	return pos
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	get_tree().change_scene_to_file("res://scenes/world.tscn")
+	get_tree().change_scene_to_file("res://scenes/nf.tscn")
