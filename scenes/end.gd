5c625dd5a5ed2e86097723d4680cf19857c417fb
lanceabellanosa <lance.abellanosa@cathcollege.school.nz>
2026-09-24 13:27:40 +1200

    godot files

---
+++ scenes/end.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/floor_5.tscn")
